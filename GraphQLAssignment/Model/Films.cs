c63a22c6a5445f13a72eb1b77b3b249c9953279d
sreekuttancl <sreekuttancl12@gmail.com>
2019-08-07 18:10:59 -0400

    Initial check-in of module GraphQLAssignment

---
+++ GraphQLAssignment/Model/Films.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GraphQLAssignment.Model
+{
+    public class Films
+    {
+       
+        public string title { get; set; }
+        public string director { get; set; }
+
+      
+    }
+}
